61f631e467addddc12530e0b72c1b7759e46dafb
Derek Sivers <derek@sivers.org>
2014-11-05 23:02:55 +1300

    ugly, unstyled, but works!

--- routes/sivers-comments.rb
+++ routes/sivers-comments.rb
@@ -11,19 +11,44 @@ class SiversCommentsWeb < Sinatra::Base
   end
 
   # TODO: https://developers.google.com/analytics/devguides/collection/analyticsjs/cross-domain
-  use Rack::Auth::Basic, "API key and pass" do |api_key, api_pass|
+  use Rack::Auth::Basic, 'API key and pass' do |api_key, api_pass|
     api_key.size == 8 && api_pass.size == 8
   end
 
   before do
-    api_key, api_pass =  Rack::Auth::Basic::Request.new(request.env)
-    @sc = A50C::SiversComments.new(api_key, api_pass)
-    @pagetitle = 'sivers comments'
+    #api_key, api_pass =  Rack::Auth::Basic::Request.new(request.env)
+    @sc = A50C::SiversComments.new('aaaaaaaa', 'bbbbbbbb')
+    @pagetitle = 'sivers-comments'
   end
 
   get '/' do
     @comments = @sc.get_comments
     erb :home
+  end
+
+  get %r{\A/comment/([0-9]+)\Z} do |id|
+    @comment = @sc.get_comment(id) || halt(404)
+    erb :edit
+  end
+
+  post %r{\A/comment/([0-9]+)\Z} do |id|
+    @sc.update_comment(id, params[:html])
+    redirect '/'
+  end
+
+  post %r{\A/comment/([0-9]+)/reply\Z} do |id|
+    @sc.reply_to_comment(id, params[:reply])
+    redirect '/'
+  end
+
+  post %r{\A/comment/([0-9]+)/delete\Z} do |id|
+    @sc.delete_comment(id)
+    redirect '/'
+  end
+
+  post %r{\A/comment/([0-9]+)/spam\Z} do |id|
+    @sc.spam_comment(id)
+    redirect '/'
   end
 
   run!
