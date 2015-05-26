979db419a22a64b12a7b74e6c3c6da0012573792
Derek Sivers <derek@sivers.org>
2015-05-26 20:27:41 +1200

    download links for ebook

--- routes/woodegg.rb
+++ routes/woodegg.rb
@@ -93,6 +93,12 @@ class WoodEgg < Sinatra::Base
 		erb :upload
 	end
 
+	get  %r{/download/([A-Z][a-zA-Z]+2014\.[a-z]+)\Z} do |filename|
+		files = %w(Asia2014.epub Asia2014.mobi Asia2014.pdf Cambodia2014.epub Cambodia2014.mobi Cambodia2014.pdf China2014.epub China2014.mobi China2014.pdf HongKong2014.epub HongKong2014.mobi HongKong2014.pdf India2014.epub India2014.mobi India2014.pdf Indonesia2014.epub Indonesia2014.mobi Indonesia2014.pdf Japan2014.epub Japan2014.mobi Japan2014.pdf Korea2014.epub Korea2014.mobi Korea2014.pdf Malaysia2014.epub Malaysia2014.mobi Malaysia2014.pdf Mongolia2014.epub Mongolia2014.mobi Mongolia2014.pdf Myanmar2014.epub Myanmar2014.mobi Myanmar2014.pdf Philippines2014.epub Philippines2014.mobi Philippines2014.pdf Singapore2014.epub Singapore2014.mobi Singapore2014.pdf SriLanka2014.epub SriLanka2014.mobi SriLanka2014.pdf Taiwan2014.epub Taiwan2014.mobi Taiwan2014.pdf Thailand2014.epub Thailand2014.mobi Thailand2014.pdf Vietnam2014.epub Vietnam2014.mobi Vietnam2014.pdf)
+		halt(404) unless files.include? filename
+		send_file "/var/www/htdocs/downloads/#{filename}"
+	end
+
 	get %r{\A/download/([0-9]+)/WoodEgg.*\Z} do |id|
 		up = @we.upload(id) || halt(404)
 		send_file "/var/www/htdocs/uploads/#{up[:filename]}",
