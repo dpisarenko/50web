82d03f773b406f4fa9ec9cb24f1074da09f9ce77
Derek Sivers <derek@sivers.org>
2015-06-09 09:08:02 -1000

    new musicthoughts CSS without PureCSS imports

--- scss.sh
+++ scss.sh
@@ -1,1 +1,1 @@
-scss -t compressed --sourcemap=none --watch scss/musicthoughts.scss:public/css/musicthoughts-201505.css
+scss -t compressed --sourcemap=none --watch scss/musicthoughts.scss:public/css/musicthoughts-201506.css
