e0429ff497cae3a16742f2ea4543da966e88b24e
Derek Sivers <derek@sivers.org>
2015-10-23 15:32:17 +1300

    new functional-style db2js.rb

--- lib/db2js.rb
+++ lib/db2js.rb
@@ -1,5 +1,48 @@
+# USAGE
+# db = getdb('live', 'peeps')
+# ok, res = db.call('tables_with_person', 1)
+# ok, res = db.call('get_stats', 'programmer', 'elm')
+# ok, res = db.call('country_count')
+# ok, res = db.call('update_person', 1, JSON.generate({email: 'boo'}))
+# if ok
+# 	puts "worked! #{res.inspect}"
+# else
+# 	puts "failed: #{res.inspect}"
+# end
 require 'pg'
 require 'json'
+
+# ONLY USE THIS: Curry calldb with a DB connection & schema
+def getdb(server, schema)
+	Proc.new do |func, *params|
+		okres(calldb(PGPool.get(server), schema, func, params))
+	end
+end
+
+# INPUT: result of pg.exec_params
+# OUTPUT: [boolean, hash] where hash is JSON of response or problem
+def okres(res)
+	js = JSON.parse(res[0]['js'], symbolize_names: true)
+	if res[0]['mime'].include? 'problem'
+		[false, {error: js[:title], message: js[:detail]}]
+	else
+		[true, js]
+	end
+end
+
+# return params string for PostgreSQL exec_params
+# INPUT: [list, of, things]
+# OUTPUT "($1,$2,$3)"
+def paramstring(params)
+	'(%s)' % (1..params.size).map {|i| "$#{i}"}.join(',')
+end
+
+# The real functional function we're going to curry, below
+# INPUT: PostgreSQL connection, schema string, function string, params array
+def calldb(pg, schema, func, params)
+	pg.exec_params('SELECT mime, js FROM %s.%s%s' %
+		[schema, func, paramstring(params)], params)
+end
 
 # PG Pool of connections. Simple as can be. Bypassed if test database.
 class PGPool
@@ -18,31 +61,3 @@ class PGPool
 	end
 end
 
-# TODO: curried function instead of initialized class?
-# All I really need is the js function.
-# Oh, but also the error and message if it fails. Hmm....
-class DB2JS
-	attr_accessor :error, :message
-
-	def initialize(schema, server='live')
-		@schema = schema
-		@pg = PGPool.get(server)
-	end
-
-	def js(func, params=[])
-		res = @pg.exec_params("SELECT mime, js FROM #{@schema + '.' + func}", params)
-		j = JSON.parse(res[0]['js'], symbolize_names: true)
-		if res[0]['mime'].include? 'problem'
-			@error = j[:title]
-			@message = j[:detail]
-			return false
-		else
-			@error = @message = nil
-			return j
-		end
-	end
-
-#	def qry(sql, params)
-#		@pg.exec_params(sql, params)
-#	end
-end
