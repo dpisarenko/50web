78cbeebdebcff4b21ec5c04cc8b77bf4fbd4e971
dp118m <dp@altruix.co>
2016-02-05 07:43:57 +0400

    Attempt at bugfix: Errno::ENOENT at /, No such file or directory @ rb_sysopen - C:/usr/dp/dev/derek/50web/routes/views/home.erb.

--- routes/songcontest.rb
+++ routes/songcontest.rb
@@ -6,10 +6,16 @@ class SongContest < Sinatra::Base
 	log = File.new('/tmp/SongContest.log', 'a+')
 	log.sync = true
 
+	configure do
+		enable :logging
+		set :root, File.dirname(File.dirname(File.realpath(__FILE__)))
+		set :views, Proc.new {File.join(root, 'views/songcontest')}
+	end
+
 
 	before do
 		env['rack.errors'] = log
-		@db = getdb('songcontest')
+		# @db = getdb('songcontest')
 	end
 
 	get '/' do
