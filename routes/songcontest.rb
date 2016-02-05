08b98f928bc17efe70576fb81387a52fc683f490
Dmitri Pisarenko <dp@altruix.co>
2016-02-05 22:28:06 +0300

    First attempt at i18n.

--- routes/songcontest.rb
+++ routes/songcontest.rb
@@ -1,5 +1,7 @@
 require 'sinatra/base'
 require 'getdb'
+require 'i18n'
+require 'i18n/backend/fallbacks'
 
 class SongContest < Sinatra::Base
 
@@ -10,13 +12,28 @@ class SongContest < Sinatra::Base
 		enable :logging
 		set :root, File.dirname(File.dirname(File.realpath(__FILE__)))
 		set :views, Proc.new {File.join(root, 'views/songcontest')}
+		I18n::Backend::Simple.send(:include, I18n::Backend::Fallbacks)
+		I18n.load_path = Dir[File.join(settings.root, 'locales', '*.yml')]
+		I18n.backend.load_translations		
 	end
-
 
 	before do
 		env['rack.errors'] = log
 		# @db = getdb('songcontest')
 	end
+
+	before '/:locale/*' do
+	  I18n.locale       =       params[:locale]
+	  request.path_info = '/' + params[:splat ][0]
+	end
+
+	helpers do
+	  def find_template(views, name, engine, &block)
+	    I18n.fallbacks[I18n.locale].each { |locale|
+	      super(views, "#{name}.#{locale}", engine, &block) }
+	    super(views, name, engine, &block)
+	  end
+	end	
 
 	get '/' do
 		erb :home
